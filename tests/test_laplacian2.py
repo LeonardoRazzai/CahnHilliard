21687571e842567469f7bd4b1aba7e7b256ea728
LeonardoRazzai <leonardo.razzai@studio.unibo.it>
2023-11-14 16:15:49 +0100

    add documentation and a test for a peak

--- tests/test_laplacian2.py
+++ tests/test_laplacian2.py
@@ -4,28 +4,57 @@ sys.path.append('./')
 sys.path.append('../')
 from CahnHilliard import laplacian2
 
-def test_laplacian2_shapes():
-    '''Test input and output shape matching'''
-    # Test case 1: Square input matrix
-    input_field = np.array([[10, 20, 30],
-                            [40, 50, 60],
-                            [70, 80, 90]])
+def test_laplacian2_input_output_shapes():
+    """
+    GIVEN an input field with all zeros
+    WHEN the laplacian2 function is applied
+    THEN the shape of the result is equal to the shape of the input.
+    """
+    input_field = np.zeros((5, 5))  # Replace with your desired input shape
     result = laplacian2(input_field)
-    assert result.shape == input_field.shape, "Test case 1 failed"
 
-    # Test case 2: Rectangular input matrix
-    input_field = np.array([[10, 20],
-                            [30, 40],
-                            [50, 60]])
+    np.testing.assert_equal(input_field.shape, result.shape, "Test Failed: Input and output shapes are not equal")
+
+
+def test_laplacian2_with_central_peak():
+    """
+    GIVEN an central peaked input field
+    WHEN the laplacian2 function is applied
+    THEN the result is the expected Laplacian-filtered output.
+    """
+    input_field = np.array([[0, 1, 0],
+                            [1, 3, 1],
+                            [0, 1, 0]])
+
+    expected_output = np.array([[ 2,  0,  2],
+                                [ 0, -8,  0],
+                                [ 2,  0,  2]])
+
     result = laplacian2(input_field)
-    assert result.shape == input_field.shape, "Test case 2 failed"
+
+    np.testing.assert_array_equal(result, expected_output, "Test Failed: Central peak")
     
-def test_constant_field():
-    '''Testing that constant input field gives all zeros result'''
-    shape = (6, 6)
-    # set same shape for input field and all zeros field
-    input_field = np.ones(shape)
-    zeros = np.zeros(shape)
+
+def test_laplacian2_with_zeros():
+    """
+    GIVEN an input field containing all zeros
+    WHEN the laplacian2 function is applied
+    THEN the result is an array of zeros.
+    """
+    input_field = np.zeros((10, 10))
     result = laplacian2(input_field)
+    expected_output = np.zeros((10, 10))
     
-    np.testing.assert_equal(zeros, result)
+    np.testing.assert_array_equal(result, expected_output, "Test Failed: Zeros input")
+
+def test_laplacian2_with_ones():
+    """
+    GIVEN an input field containing all ones
+    WHEN the laplacian2 function is applied
+    THEN the result is an array of zeros.
+    """
+    input_field = np.ones((10, 10))
+    result = laplacian2(input_field)
+    expected_output = np.zeros((10, 10))
+    
+    np.testing.assert_array_equal(result, expected_output, "Test Failed: Ones input")
